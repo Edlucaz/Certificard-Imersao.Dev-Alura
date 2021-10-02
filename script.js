7daa2f493c9bdfe56e7b1c2802755cdc7cdfd048
Lucas Rocha <91226344+Edlucaz@users.noreply.github.com>
2021-10-02 18:45:50 -0300

    Personalizando Certificard

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function mudaTema() {
-  document.body.classList.toggle("dark");
+  var temaDark = document.body.classList.toggle("dark");
+  var logoGitHub = document.getElementById("gitHub");
+  var logoCodepen = document.getElementById("codepen");
+  var botaoTema = document.getElementById("botaoTema");
+  if (temaDark == true) {
+    logoGitHub.src = "https://cdn.icon-icons.com/icons2/2429/PNG/512/github_logo_icon_147285.png";
+    logoCodepen.src = "https://blog.codepen.io/wp-content/uploads/2012/06/Button-Fill-White-Small.png";
+    botaoTema.textContent = "Modo Light";
+  } else {
+    logoGitHub.src = "https://cdn-icons-png.flaticon.com/512/25/25231.png"
+    logoCodepen.src = "https://blog.codepen.io/wp-content/uploads/2012/06/Button-Fill-Black-Large.png"
+    botaoTema.textContent = "Modo Dark";
+  }
 }
